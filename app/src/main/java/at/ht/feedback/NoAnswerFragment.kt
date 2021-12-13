be4d4bf8487f2d680f8875b4e560f9638749f447
StarkaLukas <lstarka141000@gmail.com>
2021-12-13 21:42:06 +0100

    add navigate to welcome fragment and question fragment

--- app/src/main/java/at/ht/feedback/NoAnswerFragment.kt
+++ app/src/main/java/at/ht/feedback/NoAnswerFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import at.ht.feedback.databinding.FragmentNoAnswerBinding
+import at.ht.feedback.databinding.FragmentQuestionBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +16,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NoAnswerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding: FragmentNoAnswerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_no_answer, container, false
+        )
+        binding.button2.setOnClickListener {
+                view ->
+            view.findNavController().navigate(R.id.action_noAnswerFragment_to_questionFragment)
+        }
+        binding.button4.setOnClickListener {
+                view ->
+            view.findNavController().navigate(R.id.action_noAnswerFragment_to_welcomeFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_no_answer, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoAnswerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoAnswerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }
