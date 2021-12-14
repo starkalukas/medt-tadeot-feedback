18c599c1ca32f5b794eeb1e3ec7fa8620f4ed675
StarkaLukas <lstarka141000@gmail.com>
2021-12-14 15:00:34 +0100

    add about view

--- app/src/main/java/at/ht/feedback/WelcomeFragment.kt
+++ app/src/main/java/at/ht/feedback/WelcomeFragment.kt
@@ -1,12 +1,12 @@
 package at.ht.feedback
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import at.ht.feedback.databinding.FragmentWelcomeBinding
 import kotlinx.android.synthetic.main.fragment_welcome.*
 
@@ -33,6 +33,16 @@ class WelcomeFragment : Fragment() {
                 view.findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment2)
         }
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, findNavController())
     }
 }
